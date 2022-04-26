8e401ae9f0946fdc58a3b5a890389be8e2c262d0
mattjkatz <matthewkatz7@gmail.com>
2022-04-25 20:50:47 -0500

    completed the show test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -26,15 +26,15 @@ RSpec.describe "Posts", type: :request do
 
       user = User.create!(name: "Test", email: "test@test.com", password: "password")
 
-      post = Post.create!(user_id: user.id, title: title, body: body, image: "https://i.picsum.photos/id/302/200/300.jpg?hmac=b5e6gUSooYpWB3rLAPrDpnm8PsPb84p_NXRwD-DK-1I")
+      post = Post.create!(user_id: user.id, title: Faker::Company.catch_phrase, body: Faker::Hipster.paragraph(sentence_count: 6), image: "https://i.picsum.photos/id/302/200/300.jpg?hmac=b5e6gUSooYpWB3rLAPrDpnm8PsPb84p_NXRwD-DK-1I")
 
       get "/posts/#{post.id}"
       post = JSON.parse(response.body)
 
       expect(response).to have_http_status(200)
-      expect(post.title).to eq(title)
-      expect(post.body).to eq(body)
-      expect(post.image).to eq("https://i.picsum.photos/id/302/200/300.jpg?hmac=b5e6gUSooYpWB3rLAPrDpnm8PsPb84p_NXRwD-DK-1I")
+      expect(post["title"]).to eq(post["title"])
+      expect(post["body"]).to eq(post["body"])
+
     end
   end
 
