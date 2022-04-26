5733ba2788470efa21b1eab0efa91f9a68bafbd7
mattjkatz <matthewkatz7@gmail.com>
2022-04-25 20:43:10 -0500

    testing the show test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -17,4 +17,25 @@ RSpec.describe "Posts", type: :request do
       expect(posts.length).to eq(3)
     end
   end
+
+  describe "GET /posts/:id" do
+    it "should return the attributes of one specific post" do
+
+      title = Faker::Company.catch_phrase
+      body = Faker::Hipster.paragraph(sentence_count: 6)
+
+      user = User.create!(name: "Test", email: "test@test.com", password: "password")
+
+      post = Post.create!(user_id: user.id, title: title, body: body, image: "https://i.picsum.photos/id/302/200/300.jpg?hmac=b5e6gUSooYpWB3rLAPrDpnm8PsPb84p_NXRwD-DK-1I")
+
+      get "/posts/#{post.id}"
+      post = JSON.parse(response.body)
+
+      expect(response).to have_http_status(200)
+      expect(post.title).to eq(title)
+      expect(post.body).to eq(body)
+      expect(post.image).to eq("https://i.picsum.photos/id/302/200/300.jpg?hmac=b5e6gUSooYpWB3rLAPrDpnm8PsPb84p_NXRwD-DK-1I")
+    end
+  end
+
 end
