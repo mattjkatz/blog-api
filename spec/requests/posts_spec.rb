9e6a3d363000839d3c5d1ba9c9c374fbf9a4bd91
mattjkatz <matthewkatz7@gmail.com>
2022-04-25 19:47:30 -0500

    add rspec testing

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+  describe "GET /posts" do
+    it "return blog posts" do
+
+      user = User.create!(name: "Test", email: "test@test.com", password: "password")
+
+      Post.create!(user_id: user.id, title: Faker::Company.catch_phrase, body: Faker::Hipster.paragraph(sentence_count: 6), image: "https://i.picsum.photos/id/302/200/300.jpg?hmac=b5e6gUSooYpWB3rLAPrDpnm8PsPb84p_NXRwD-DK-1I")
+      Post.create!(user_id: user.id, title: Faker::Company.catch_phrase, body: Faker::Hipster.paragraph(sentence_count: 6), image: "https://i.picsum.photos/id/131/200/300.jpg?hmac=9q7mRSOguNBFGg_gPPRKlfjNINGjXWeDBTYPP1_gEas")
+      Post.create!(user_id: user.id, title: Faker::Company.catch_phrase, body: Faker::Hipster.paragraph(sentence_count: 6), image: "https://i.")
+
+      get "/posts"
+      posts = JSON.parse(response.body)
+      
+      expect(response).to have_http_status(200)
+      expect(posts.length).to eq(3)
+    end
+  end
+end
